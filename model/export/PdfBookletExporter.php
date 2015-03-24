0fc766d56d6822b8b7a543f0897f4989700e7392
hutnikau <hutnikau@1pt.com>
2015-03-24 16:21:19 +0300

    TAO-510: PDF generation from test

---
+++ model/export/PdfBookletExporter.php
@@ -0,0 +1,116 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2014 (original work) Open Assessment Technologies SA;
+ *               
+ */
+
+namespace oat\taoBooklet\model\export;
+
+use mikehaertl\wkhtmlto\Pdf;
+
+/**
+ * PdfBookletExporter provides functionality to export booklet to 
+ * PDF format (directly send to end user or save into pdf file).
+ *
+ * @author Aleh Hutnikau <hutnikau@1pt.com>
+ */
+class PdfBookletExporter extends BookletExporter
+{
+    /**
+     * @var Pdf instance of the wrapper around wkhtmltopdf
+     */
+    protected $pdf;
+    
+    public function __construct()
+    {
+        $this->pdf = new Pdf;
+    }
+    
+    /**
+     * Send PDF to client, either inline display or as download a file
+     * 
+     * @param string|null $filename the filename to send. If empty, the PDF is streamed inline.
+     * @return bool whether PDF was created successfully
+     */
+    public function export($filename = null) 
+    {
+        $this->pdf->addPage($this->_content);
+        return $this->pdf->send($filename);
+    }
+    
+    /**
+     * Set booklet content
+     * 
+     * @param string $content either a URL, a HTML string or a PDF/HTML filename
+     * @return PdfBookletExporter instance for method chaining
+     * @throws BookletExporterException if wrong $content parameter passed
+     */
+    public function setContent($content) 
+    {   
+        $result = '';
+        if (filter_var($content, FILTER_VALIDATE_URL)) { //url
+            $opts = array( 
+                'http'=>array( 
+                    'method'=>"GET",
+                    'Cookie: ' . session_name() . '=' . session_id() . "\r\n" 
+                ) 
+            );
+            $context = stream_context_create($opts);
+            $result = $file = file_get_contents($content, false, $context);
+        } elseif (file_exists($content) && is_file($content)) {  //file path
+            $result = file_get_contents($content);
+        } else { //HTML string
+            $result = $content;
+        }
+        if (is_string($result)) {
+            $this->_content = $result;
+            return $this;
+        }
+        throw new BookletExporterException('Wrong content type');
+    }
+    
+    /**
+     * Set Pdf option(s)
+     *
+     * @param array $options list of global PDF options to set as name/value pairs
+     * @return PdfBookletExporter instance for method chaining
+     */
+    public function setOptions($options = array()) {
+        $this->pdf->setOptions($options);
+        return $this;
+    }
+    
+    /**
+     * Save booklet into file
+     * 
+     * @param string $path the file path
+     * @return bool whether file was created successfully
+     */
+    public function saveAs($path)
+    {
+        $this->pdf->addPage($this->_content);
+        return $this->pdf->saveAs($path);
+    }
+    
+    /**
+     * @return string the detailed error message. Empty string if none.
+     */
+    public function getError()
+    {
+        return $this->pdf->getError();
+    }
+}
---
+++ test/PdfBookletExporterTest.php
@@ -0,0 +1,120 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2014 (original work) Open Assessment Technologies SA;
+ *               
+ */
+
+namespace oat\taoBooklet\test;
+
+use oat\tao\test\TaoPhpUnitTestRunner;
+use oat\taoBooklet\model\export\PdfBookletExporter;
+
+/**
+ * 
+ * @author Aleh Hutnikau, <hutnikau@1pt.com>
+ * @package taoBooklet
+ */
+class PdfBookletExporterTest extends TaoPhpUnitTestRunner
+{
+    /**
+     * @var PdfBookletExporter exporter instance
+     */
+    private $pdfBookletExporter;
+    
+    /**
+     * @var string sample html file content
+     */
+    private $HTMLString = '<html>
+                <head></head>
+                <body>Body content</body>
+            </html>';
+    
+    
+    /**
+     * tests initialization
+     */
+    public function setUp()
+    {
+        TaoPhpUnitTestRunner::initTest();
+        $this->pdfBookletExporter = new PdfBookletExporter();
+    }
+    
+    /**
+     * Check different types of input (url, Html string, filepath)
+     */
+    public function testSetContent() 
+    {
+        $HTMLFilePath = __DIR__ . DIRECTORY_SEPARATOR . 'samples' . DIRECTORY_SEPARATOR . 'bookletSample.html';
+        //set document content as a string
+        $this->assertTrue($this->pdfBookletExporter->setContent($this->HTMLString) instanceof PdfBookletExporter);
+        $this->assertTrue($this->pdfBookletExporter->getContent() == $this->HTMLString);
+        
+        //set document content as a filepath
+        $this->pdfBookletExporter->setContent($HTMLFilePath);
+        $this->assertTrue($this->pdfBookletExporter->getContent() == file_get_contents($HTMLFilePath));
+        
+        $this->pdfBookletExporter->setContent('');
+        $this->assertTrue($this->pdfBookletExporter->getContent() == '');
+        
+        //set document content as a url (internet connection required)
+        $this->pdfBookletExporter->setContent('http://google.com');
+        $this->assertTrue($this->pdfBookletExporter->getContent() != '');
+    }
+    
+    /**
+     * @expectedException oat\taoBooklet\model\export\BookletExporterException
+     * @expectedExceptionMessage Wrong content type
+     */
+    public function testSetContentException() 
+    {
+        $this->pdfBookletExporter->setContent(null);
+    }
+    
+    /**
+     * Check pdf file export
+     * 
+     * @preserveGlobalState disabled
+     * @runInSeparateProcess
+     */
+    public function testExport()
+    {
+        $name = 'filename';
+        
+        $this->pdfBookletExporter->setContent($this->HTMLString);
+        ob_start();
+        $this->assertTrue($this->pdfBookletExporter->export($name));
+        $pdfFileContent = ob_get_clean();
+        
+        $headers = xdebug_get_headers();
+        
+        $this->assertStringStartsWith('%PDF', $pdfFileContent);
+        $this->assertTrue(in_array('Content-Type: application/pdf', $headers));
+    }
+    
+    /**
+     * Check pdf file saving
+     */
+    public function testSaveAs()
+    {
+        $pdfFilePath = __DIR__ . DIRECTORY_SEPARATOR . 'samples' . DIRECTORY_SEPARATOR . 'bookletSample.pdf';
+        $this->pdfBookletExporter->setContent($this->HTMLString);
+        $this->assertTrue($this->pdfBookletExporter->saveAs($pdfFilePath));
+        $this->assertFileExists($pdfFilePath);
+        unlink($pdfFilePath);
+    }
+    
+}
