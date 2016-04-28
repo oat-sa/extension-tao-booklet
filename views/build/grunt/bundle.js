cb37266425009fadafb7cfdf3b7809540a4ab3c5
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-04-28 11:51:30 +0200

    remove mediaelement

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
             modules : [{
                 name: 'taoBooklet/controller/routes',
                 include : ext.getExtensionsControllers(['taoBooklet']),
-                exclude : ['mathJax', 'mediaElement', 'css!taoQtiPrintCss/qti.css'].concat(libs)
+                exclude : ['mathJax', 'css!taoQtiPrintCss/qti.css'].concat(libs)
             }]
         }
     };
