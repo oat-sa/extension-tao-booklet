2a29bf793338febb8768e00ba9edfae01e380642
jsconan <jsconan@gmail.com>
2017-04-19 09:47:38 +0200

    Extract the booklet config from the properties

--- model/BookletConfigService.php
+++ model/BookletConfigService.php
@@ -23,6 +23,7 @@
 
 namespace oat\taoBooklet\model;
 
+use core_kernel_classes_Resource;
 use oat\oatbox\service\ConfigurableService;
 
 class BookletConfigService extends ConfigurableService
@@ -32,6 +33,58 @@ class BookletConfigService extends ConfigurableService
     const OPTION_DEFAULT_VALUES = 'default_values';
     const OPTION_MENTION = 'mention';
     const OPTION_LINK = 'link';
+
+    const CONFIG_LAYOUT = 'layout';
+    const CONFIG_COVER_PAGE = 'cover_page';
+    const CONFIG_PAGE_HEADER = 'page_header';
+    const CONFIG_PAGE_FOOTER = 'page_footer';
+    const CONFIG_ONE_PAGE_ITEM = 'one_page_item';
+    const CONFIG_ONE_PAGE_SECTION = 'one_page_section';
+    const CONFIG_TITLE = 'title';
+    const CONFIG_SECTION = 'section';
+    const CONFIG_DESCRIPTION = 'description';
+    const CONFIG_DATE = 'date';
+    const CONFIG_LOGO = 'logo';
+    const CONFIG_QRCODE = 'qr_code';
+    const CONFIG_MENTION = 'mention';
+    const CONFIG_LINK = 'link';
+    const CONFIG_PAGE_NUMBER = 'page_number';
+
+    /**
+     * Maps the properties to config names
+     * @var array
+     */
+    protected $configMap = [
+        // properties
+        BookletClassService::PROPERTY_LAYOUT => self::CONFIG_LAYOUT,
+        BookletClassService::PROPERTY_COVER_PAGE => self::CONFIG_COVER_PAGE,
+        BookletClassService::PROPERTY_PAGE_HEADER => self::CONFIG_PAGE_HEADER,
+        BookletClassService::PROPERTY_PAGE_FOOTER => self::CONFIG_PAGE_FOOTER,
+
+        // values
+        BookletClassService::INSTANCE_LAYOUT_COVER => self::CONFIG_COVER_PAGE,
+        BookletClassService::INSTANCE_LAYOUT_HEADER => self::CONFIG_PAGE_HEADER,
+        BookletClassService::INSTANCE_LAYOUT_FOOTER => self::CONFIG_PAGE_FOOTER,
+        BookletClassService::INSTANCE_LAYOUT_ONE_PAGE_ITEM => self::CONFIG_ONE_PAGE_ITEM,
+        BookletClassService::INSTANCE_LAYOUT_ONE_PAGE_SECTION => self::CONFIG_ONE_PAGE_SECTION,
+
+        BookletClassService::INSTANCE_COVER_PAGE_TITLE => self::CONFIG_TITLE,
+        BookletClassService::INSTANCE_COVER_PAGE_DESCRIPTION => self::CONFIG_DESCRIPTION,
+        BookletClassService::INSTANCE_COVER_PAGE_DATE => self::CONFIG_DATE,
+        BookletClassService::INSTANCE_COVER_PAGE_LOGO => self::CONFIG_LOGO,
+        BookletClassService::INSTANCE_COVER_PAGE_QRCODE => self::CONFIG_QRCODE,
+
+        BookletClassService::INSTANCE_HEADER_LOGO => self::CONFIG_LOGO,
+        BookletClassService::INSTANCE_HEADER_TITLE => self::CONFIG_TITLE,
+        BookletClassService::INSTANCE_HEADER_SECTION => self::CONFIG_SECTION,
+        BookletClassService::INSTANCE_HEADER_PAGE_NUMBER => self::CONFIG_PAGE_NUMBER,
+
+        BookletClassService::INSTANCE_FOOTER_MENTION => self::CONFIG_MENTION,
+        BookletClassService::INSTANCE_FOOTER_LINK => self::CONFIG_LINK,
+        BookletClassService::INSTANCE_FOOTER_TITLE => self::CONFIG_TITLE,
+        BookletClassService::INSTANCE_FOOTER_SECTION => self::CONFIG_SECTION,
+        BookletClassService::INSTANCE_FOOTER_PAGE_NUMBER => self::CONFIG_PAGE_NUMBER,
+    ];
 
     /**
      * Set the default values onto the provided form
@@ -56,5 +109,64 @@ class BookletConfigService extends ConfigurableService
                 }
             }
         }
+    }
+
+    /**
+     * Gets the config for a booklet instance
+     * @param core_kernel_classes_Resource $instance
+     * @return array
+     */
+    public function getConfig(core_kernel_classes_Resource $instance)
+    {
+        $properties = $instance->getPropertiesValues([
+            BookletClassService::PROPERTY_LAYOUT,
+            BookletClassService::PROPERTY_COVER_PAGE,
+            BookletClassService::PROPERTY_PAGE_HEADER,
+            BookletClassService::PROPERTY_PAGE_FOOTER
+        ]);
+
+        $config = [
+            self::CONFIG_LAYOUT => [],
+            self::CONFIG_COVER_PAGE => [],
+            self::CONFIG_PAGE_HEADER => [],
+            self::CONFIG_PAGE_FOOTER => [],
+            self::CONFIG_MENTION => $this->getOption(self::OPTION_MENTION),
+            self::CONFIG_LINK => $this->getOption(self::OPTION_LINK),
+        ];
+
+
+        if (isset($properties[BookletClassService::PROPERTY_LAYOUT])) {
+            $config[self::CONFIG_LAYOUT] = $this->getConfigSet($properties[BookletClassService::PROPERTY_LAYOUT]);
+        }
+        if (isset($properties[BookletClassService::PROPERTY_COVER_PAGE])) {
+            $config[self::CONFIG_COVER_PAGE] = $this->getConfigSet($properties[BookletClassService::PROPERTY_COVER_PAGE]);
+        }
+        if (isset($properties[BookletClassService::PROPERTY_PAGE_HEADER])) {
+            $config[self::CONFIG_PAGE_HEADER] = $this->getConfigSet($properties[BookletClassService::PROPERTY_PAGE_HEADER]);
+        }
+        if (isset($properties[BookletClassService::PROPERTY_PAGE_FOOTER])) {
+            $config[self::CONFIG_PAGE_FOOTER] = $this->getConfigSet($properties[BookletClassService::PROPERTY_PAGE_FOOTER]);
+        }
+
+        return $config;
+    }
+
+    /**
+     * Translates a list of resource properties into a list of named boolean values
+     * @param array $properties
+     * @return array
+     */
+    protected function getConfigSet($properties)
+    {
+        $config = [];
+
+        foreach($properties as $property) {
+            $uri = $property->getUri();
+            if (isset($this->configMap[$uri])) {
+                $config[$this->configMap[$uri]] = true;
+            }
+        }
+
+        return $config;
     }
 }
