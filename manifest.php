17d819295534454f5a31527e9926a4ae7522b205
jsconan <jsconan@gmail.com>
2017-07-26 10:12:37 +0200

    Bump version

--- manifest.php
+++ manifest.php
@@ -24,12 +24,12 @@ return array(
     'label'       => 'Test Booklets',
     'description' => 'An extension for TAO to create test booklets (publishable in MS-Word and PDF along with Answer Sheets)',
     'license'     => 'GPL-2.0',
-    'version'     => '1.6.0',
+    'version'     => '1.7.0',
     'author'      => 'Open Assessment Technologies SA',
     'requires'    => array(
         'tao'          => '>=10.2.0',
         'taoQtiTest'   => '>=7.0.0',
-        'taoQtiPrint'  => '>=1.2.0',
+        'taoQtiPrint'  => '>=1.4.0',
         'taoOutcomeUi' => '>=4.5.0',
     ),
     // for compatibility
