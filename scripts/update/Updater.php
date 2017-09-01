4abd8ba5653e06896d2eb12ff4920bd71690d2f0
Dieter Raber <dieter@taotesting.com>
2017-09-01 12:32:02 +0200

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -170,6 +170,6 @@ class Updater extends \common_ext_ExtensionUpdater {
             $this->setVersion('1.9.1');
         }
 
-        $this->skip('1.9.1', '1.9.2');
+        $this->skip('1.9.1', '1.9.3');
     }
 }
