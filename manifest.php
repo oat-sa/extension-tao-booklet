f66e12914217f5ab6bf9129692d946b98ff416fa
Sam Sipasseuth <sam@taotesting.com>
2017-10-06 15:44:13 +0200

    updated dep version to taoQtiPrint

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'requires'    => array(
         'tao'          => '>=10.2.0',
         'taoQtiTest'   => '>=7.0.0',
-        'taoQtiPrint'  => '>=1.5.0',
+        'taoQtiPrint'  => '>=1.6.0',
         'taoOutcomeUi' => '>=4.5.0',
     ),
     // for compatibility
