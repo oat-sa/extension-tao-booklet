783b0e5fdef78bee39e06633c8888d3b5f64372d
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-10-08 16:01:34 +0200

    migrate to atomic bundles

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -1,58 +1,44 @@
-module.exports = function(grunt) {
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2018 (original work) Open Assessment Technologies SA;
+ */
 
+/**
+ * configure the extension bundles
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+module.exports = function(grunt) {
     'use strict';
 
-    var requirejs   = grunt.config('requirejs') || {};
-    var clean       = grunt.config('clean') || {};
-    var copy        = grunt.config('copy') || {};
-
-    var root        = grunt.option('root');
-    var libs        = grunt.option('mainlibs');
-    var ext         = require(root + '/tao/views/build/tasks/helpers/extensions')(grunt, root);
-    var out         = 'output';
-
-    /**
-     * Remove bundled and bundling files
-     */
-    clean.taobookletbundle = [out];
-
-    /**
-     * Compile tao files into a bundle
-     */
-    requirejs.taobookletbundle = {
-        options: {
-            baseUrl : '../js',
-            dir : out,
-            mainConfigFile : './config/requirejs.build.js',
-            paths : {
-                'taoBooklet' : root + '/taoBooklet/views/js',
-                'taoQtiPrint' : root + '/taoQtiPrint/views/js',
-                'taoQtiPrintCss' : root + '/taoQtiPrint/views/css',
-                'taoQtiItem' : root + '/taoQtiItem/views/js',
-                'taoItems' : root + '/taoItems/views/js'
-            },
-            modules : [{
-                name: 'taoBooklet/controller/routes',
-                include : ext.getExtensionsControllers(['taoBooklet']),
-                exclude : ['mathJax', 'css!taoQtiPrintCss/qti.css'].concat(libs)
-            }]
+    grunt.config.merge({
+        bundle : {
+            taobooklet : {
+                options : {
+                    extension : 'taoBooklet',
+                    outputDir : 'loader',
+                    dependencies : ['taoQtiPrint', 'taoItems', 'taoQtiItem'],
+                    bundles : [{
+                        name : 'taoBooklet',
+                        default : true
+                    }]
+                }
+            }
         }
-    };
-
-    /**
-     * copy the bundles to the right place
-     */
-    copy.taobookletbundle = {
-        files: [
-            { src: [out + '/taoBooklet/controller/routes.js'],  dest: root + '/taoBooklet/views/js/controllers.min.js' },
-            { src: [out + '/taoBooklet/controller/routes.js.map'],  dest: root + '/taoBooklet/views/js/controllers.min.js.map' }
-        ]
-    };
-
-    grunt.config('clean', clean);
-    grunt.config('requirejs', requirejs);
-    grunt.config('copy', copy);
+    });
 
     // bundle task
-    grunt.registerTask('taobookletbundle', ['clean:taobookletbundle', 'requirejs:taobookletbundle', 'copy:taobookletbundle']);
+    grunt.registerTask('taobookletbundle', ['bundle:taobooklet']);
 };
