c913304b7f55d59e534b54638c70b6ea0d59a3f8
Pavel Handleman <pavel.handleman@gmail.com>
2019-03-13 12:03:03 +0300

    TAO-7629 eslint fix

--- views/js/test/runner/testRunner/test.js
+++ views/js/test/runner/testRunner/test.js
@@ -1,29 +1,29 @@
-define( [
+define([
 
     'jquery',
     'taoBooklet/runner/testRunner',
     'json!taoBooklet/test/samples/test.json'
-], function(  $, testRunner, testData ) {
+], function($, testRunner, testData) {
 
     var container = 'outside-container';
 
-    QUnit.module( 'Runner API' );
+    QUnit.module('Runner API');
 
-    QUnit.test( 'module', function( assert ) {
-        assert.ok( typeof testRunner !== 'undefined', 'The module exports something' );
-        assert.ok( typeof testRunner === 'function', 'The module exports a function' );
-    } );
+    QUnit.test('module', function(assert) {
+        assert.ok(typeof testRunner !== 'undefined', 'The module exports something');
+        assert.ok(typeof testRunner === 'function', 'The module exports a function');
+    });
 
-    QUnit.module( 'Render' );
+    QUnit.module('Render');
 
-    QUnit.test( 'module', function( assert ) {
+    QUnit.test('module', function(assert) {
         var ready = assert.async();
-        assert.expect( 1 );
-        testRunner( $( '.' + container ), testData );
-        setTimeout( function() {
-            assert.ok( true );
+        assert.expect(1);
+        testRunner($('.' + container), testData);
+        setTimeout(function() {
+            assert.ok(true);
             ready();
-        }, 1000 );
-    } );
-} );
+        }, 1000);
+    });
+});
 
