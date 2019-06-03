3b83de29c131fbf1572968967f6c6d6bd9d2f781
Anshul Sharma <ansul@taotesting.com>
2019-06-03 15:38:42 +0200

    new test

--- views/js/test/taskQueue/test.js
+++ views/js/test/taskQueue/test.js
@@ -1,29 +1,13 @@
 define([
-
     'jquery',
-    'taoBooklet/runner/testRunner',
-    'json!taoBooklet/test/samples/test.json'
-], function($, testRunner, testData) {
-
-    var container = 'outside-container';
-
+    'util/url',
+    'taoBooklet/component/taskQueue'
+], function($, urlHelper, taskQueueFactory) {
     QUnit.module('Runner API');
 
     QUnit.test('module', function(assert) {
-        assert.ok(typeof testRunner !== 'undefined', 'The module exports something');
-        assert.ok(typeof testRunner === 'function', 'The module exports a function');
-    });
-
-    QUnit.module('Render');
-
-    QUnit.test('module', function(assert) {
-        var ready = assert.async();
-        assert.expect(1);
-        testRunner($('.' + container), testData);
-        setTimeout(function() {
-            assert.ok(true);
-            ready();
-        }, 1000);
+        assert.ok(typeof taskQueueFactory !== 'undefined', 'The module exports something');
+        assert.ok(typeof taskQueueFactory === 'function', 'The module exports a function');
     });
 });
 
