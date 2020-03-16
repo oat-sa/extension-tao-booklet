072148aae52bcbcebf685ff6de45050798403caf
Razhkou <yuryr@taotesting.com>
2020-03-16 16:08:12 +0300

    bump. upgrade to phpunit8

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return [
     'label'       => 'Test Booklets',
     'description' => 'An extension for TAO to create test booklets (publishable in MS-Word and PDF along with Answer Sheets)',
     'license'     => 'GPL-2.0',
-    'version'     => '3.3.0',
+    'version'     => '3.4.0',
     'author'      => 'Open Assessment Technologies SA',
     'requires'    => [
         'tao'          => '>=30.0.0',
